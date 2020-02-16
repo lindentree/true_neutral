bb6dbf75e0b89fc49d7bd07c626a02f95236f9ef
Linden Chiu <hchiu87@gmail.com>
2020-02-15 17:34:20 -0800

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -53021,7 +53021,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54397" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55034" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
